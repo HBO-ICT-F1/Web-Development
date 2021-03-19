4a2737cd142294daa08a5e782b23145d4ee252f2
Robert <robertkremer.frl@gmail.com>
2021-03-19 18:31:29 +0100

    Added database Downgraded EntityFramework to 5.0.4 Added database model for users Added WIP Database.cs class Combined Program.cs and Startup.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Web_Development
 {
@@ -13,14 +15,48 @@ namespace Web_Development
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // Configure host builder
+            var builder = Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(hostBuilder => hostBuilder.UseStartup<Program>());
+
+            // Build ASP.NET host
+            var host = builder.Build();
+
+            // Make sure the database exists before running ASP.NET server
+            if (!Database.Create(host)) return;
+            host.Run();
+        }
+        
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            // Add database context to project
+            services.AddDbContext<Database>();
+
+            // Add razor pages to project
+            services.AddRazorPages();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // Set ASP.NET preferences
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            // Add endpoints
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => endpoints.MapRazorPages());
+        }
     }
 }
