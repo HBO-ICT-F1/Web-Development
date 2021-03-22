79baaf15b67e7b09a0db9ceb00e48f29dea98202
Rutger <32477913+LunkLoafGrumble@users.noreply.github.com>
2021-03-22 22:39:56 +0100

    add basic register system

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Web_Development.Models;
+using static BCrypt.Net.BCrypt;
 
 namespace Web_Development.Controllers
 {
     public class AuthController : Controller
     {
-       [HttpGet("/login")]
+        [HttpGet("/login")]
         public IActionResult Login()
         {
             return View("Login");
@@ -14,6 +17,36 @@ namespace Web_Development.Controllers
         public IActionResult Register()
         {
             return View("Register");
+        }
+        
+        
+        [HttpPost("/register")]
+        public IActionResult CreateUser()
+        {
+            var name = Request.Form["Name"];
+            var email = Request.Form["Email"];
+            var password = Request.Form["Password"];
+            var address = Request.Form["Address"];
+            var postCode = Request.Form["PostalCode"];
+            var country = Request.Form["Country"];
+            var hash = EnhancedHashPassword(password);
+            using (var database = new Database())
+            {
+                var user = new User
+                {
+                    Name = name,
+                    Email = email,
+                    Password = hash,
+                    Address = address,
+                    PostalCode = postCode,
+                    Country = country,
+                    Role = 0,
+                    CreatedAt = DateTime.Today
+                };
+                database.Add(user);
+                database.SaveChanges();
+            }
+            return Redirect("/login");
         }
     }
 }
