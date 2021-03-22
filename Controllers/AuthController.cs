ba755adbde8fb67239e068730d2c93574fa730ee
Rutger <32477913+LunkLoafGrumble@users.noreply.github.com>
2021-03-22 21:46:00 +0100

    add Controllers

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Development.Controllers
+{
+    public class AuthController : Controller
+    {
+       [HttpGet("/login")]
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+        
+        [HttpGet("/register")]
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+    }
+}
