7801bfd062148af697eb6da3d1a70815375720e3
TheDutchScorpion <kevinsliedrecht2000@gmail.com>
2021-03-23 21:58:49 +0100

    Add database products loaded on catalogue page

--- Controllers/CatalogueController.cs
+++ Controllers/CatalogueController.cs
@@ -1,12 +1,24 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_Development.Models;
 
 namespace Web_Development.Controllers
 {
     public class CatalogueController : Controller
     {
+        private readonly Database _database;
+
+        public CatalogueController(Database database)
+        {
+            _database = database;
+        }
+
         [HttpGet("/catalogue")]
         public IActionResult Index()
         {
+            ViewBag.products = _database.Products.Include(p => p.Record);
             return View();
         }
     }
