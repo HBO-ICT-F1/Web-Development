790bd8e3b850fda7342f7c5ee7c4e0b662176497
TheDutchScorpion <kevinsliedrecht2000@gmail.com>
2021-03-23 23:15:15 +0100

    Add product page and link record card to it

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_Development.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly Database _database;
+
+        public ProductController(Database database)
+        {
+            _database = database;
+        }
+        
+        [HttpGet("/product/{ProductId}")]
+        public IActionResult Index(int ProductId)
+        {
+            ViewBag.product = _database.Products
+                .Include(p => p.Record)
+                .FirstOrDefault(b => b.Id == ProductId);
+            return View("Index");
+        }
+    }
+}
