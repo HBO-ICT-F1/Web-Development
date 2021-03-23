5eae2e6e1d30314ad6b4b13b2e58ad180f7dd6f2
Rutger <32477913+LunkLoafGrumble@users.noreply.github.com>
2021-03-23 21:55:09 +0100

    improve CatalogueController.cs

--- Controllers/CatalogueController.cs
+++ Controllers/CatalogueController.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Web_Development.Controllers
 {
     public class CatalogueController : Controller
     {
+        private readonly Database _database;
+
+        public CatalogueController(Database database)
+        {
+            _database = database;
+        }
+
         [HttpGet("/catalogue")]
         public IActionResult Index()
         {
+            ViewBag.products = _database.Products.Include(product => product.Record );
             return View();
         }
     }
