a07292652628a1976d8fe066bb7edaf33535640c
TheDutchScorpion <kevinsliedrecht2000@gmail.com>
2021-03-23 16:16:22 +0100

    Add navbar and footer styling

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,1 +1,25 @@
-﻿
+﻿$(document).ready(function() {
+    onWindowResize();
+    $(window).resize(onWindowResize);
+    $('.navbar-toggle').click(onNavbarToggle);
+});
+
+function onWindowResize() {
+    var navbar = $("nav.navbar");
+    if ($(this).width() < 768) {
+        navbar.addClass('mobile');
+    }
+    if ($(this).width() >= 768 && navbar.hasClass('mobile')) {
+        navbar.removeClass('mobile');
+        navbar.removeClass('active');
+    }
+}
+
+function onNavbarToggle() {
+    var navbar = $("nav.navbar");
+    if (navbar.hasClass('active')) {
+        navbar.removeClass('active');
+        return;
+    }
+    navbar.addClass('active');
+}
