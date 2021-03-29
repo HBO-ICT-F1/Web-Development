702b32262ab34120cde3d19b49cba5bec1a63ee6
TheDutchScorpion <kevinsliedrecht2000@gmail.com>
2021-03-29 16:51:30 +0200

    Add password change logic

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,7 +1,10 @@
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Web_Development.Middleware.Auth;
 using Web_Development.Utils;
+using static BCrypt.Net.BCrypt;
 
 namespace Web_Development.Controllers
 {
@@ -18,7 +21,34 @@ namespace Web_Development.Controllers
         [HttpGet("/account"), MiddlewareFilter(typeof(AuthMiddlewareConfig))]
         public IActionResult Index()
         {
+            ViewBag.purchasableProducts = _database.Products
+                .Include(product => product.Record)
+                .Where(product => product.ForSale)
+                .OrderByDescending(product => product.Id);
+            
+            ViewBag.soldProducts = _database.Products
+                .Include(product => product.Record)
+                .Where(product => !product.ForSale)
+                .OrderByDescending(product => product.Id);
             return View("Index");
+        }
+
+        [HttpPost("/account/password"), MiddlewareFilter(typeof(AuthMiddlewareConfig))]
+        public IActionResult PasswordChange()
+        {
+            var oldPassword = Request.Form["Password"];
+            var newPassword = Request.Form["NewPassword"];
+            if (_user == null || !EnhancedVerify(oldPassword, _user.Password))
+            {
+                TempData["error_password"] = "Het ingevulde wachtwoord is onjuist";
+                return RedirectToAction("Index");
+            }
+            
+            _user.Password = EnhancedHashPassword(newPassword);
+            _database.Users.Update(_user);
+            _auth.Login(_user);
+            _database.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- Utils/BaseController.cs
+++ Utils/BaseController.cs
@@ -2,22 +2,25 @@ using System;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Web_Development.Models;
 
 namespace Web_Development.Utils
 {
     public class BaseController : Controller
     {
         public Auth _auth;
+        public User _user;
 
         public BaseController(IHttpContextAccessor httpContextAccessor)
         {
             _auth = new Auth(httpContextAccessor.HttpContext?.Response);
+            _user = _auth.User();
         }
 
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             ViewBag.auth = _auth;
-            ViewBag.user = _auth.User();
+            ViewBag.user = _user;
             base.OnActionExecuting(filterContext);
         }
     }
